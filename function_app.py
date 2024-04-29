724bbab0edb6c4df8cf4492a75d33fb936eb1ffb
TechniqueIsKey <btcsg7@icloud.com>
2024-04-28 22:36:57 -0700

    updated api to support the other currencies and deployed
    updated api to support the other currencies and deployed

--- function_app.py
+++ function_app.py
@@ -4,16 +4,16 @@ from requests import Session
 import json
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
-
+# Function to retrieve cryptocurrency prices
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    # API details
-    url = 'http://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-    api = '3388b957-7571-4ccd-8c6d-d1ade4e83c26'  # Be sure to secure your API keys appropriately in production
+    # API url and api key
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    api = '3388b957-7571-4ccd-8c6d-d1ade4e83c26'
     parameters = {
-        'slug': 'bitcoin',
+        'slug': 'bitcoin,xrp,dogecoin,bitcoin-cash,chainlink,solana,bnb,ethereum,cardano,hedera',
         'convert': 'USD'
     }
     headers = {
@@ -23,18 +23,26 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
 
     session = Session()
     session.headers.update(headers)
+    # Send request to CoinMarketCap API
     response = session.get(url, params=parameters)
+    # Load JSON data
     data = json.loads(response.text)
     
-    # Extracting Bitcoin price
-    bitcoin_price = data['data']['1']['quote']['USD']['price']
-
     # Prepare JSON response
-    if bitcoin_price:
-        result = json.dumps({"bitcoin_price": bitcoin_price})
-        return func.HttpResponse(result, mimetype="application/json")
+    result = {}
+    # Extracting cryptocurrency prices by going over the data response
+    for crypto in data['data'].values():
+        # Extracting cryptocurrency price
+        crypto_price = crypto['quote']['USD']['price']
+        # Adding cryptocurrency price to the result
+        result[crypto['slug'] + "_price"] = crypto_price
+    # Return JSON response in organized format
+    # Mimetype displays the response in JSON format
+    if result:
+        return func.HttpResponse(json.dumps(result), mimetype="application/json")
     else:
+        # Case for failure
         return func.HttpResponse(
-            "Failed to retrieve Bitcoin price.",
+            "Failed to retrieve cryptocurrency prices.",
             status_code=500
         )
